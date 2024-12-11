b46a8e2899d875b29cd5fe6b7103b17cab03ceae
HwangHyeonseok <hhs0995@naver.com>
2024-12-12 00:05:01 +0900

    20240826_fix:WARNING:discord.gateway:Shard ID None heartbeat blocked for more than N seconds. error 해결

--- discord_notice_bot.py
+++ discord_notice_bot.py
@@ -1,5 +1,7 @@
-# 안정화 버전 (stable version - 20240818)
-# 실행 명령어 : pm2 start 20240810.py --interpreter python3
+# WARNING:discord.gateway:Shard ID None heartbeat blocked for more than N seconds. 오류 해결
+
+# 안정화 버전 (stable version - 20240826)
+# 실행 명령어 : pm2 start 20240826.py --interpreter python3
 
 # --------------- 필 독 ------------------
 # # 주의1 : 실행하고 종료한 경우
@@ -14,16 +16,18 @@
 # Google Chrome 브라우저 버전 128.0.6613.84 (google-chrome --version)
 # ChromeDriver 버전 128.0.6613.84 (cd chromedriver-linux64/ 후에 ./chromedriver --version)
 # ---------------------------------------------
-import chromedriver_autoinstaller
-import random
-import copy
 import asyncio
-from user_agents import parse
+import aiohttp
+from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from discord.ext import commands, tasks
 import discord
+import random
+import copy
+import chromedriver_autoinstaller
 
+# chromedriver 자동 설치
 chromedriver_autoinstaller.install()
 
 # ========================================== config.json ===========================================
@@ -70,6 +74,7 @@ channel = None  # 채널 변수를 전역 변수로 설정
 
 # ========================================== 함수 ===================================================
 def make_user_agent(ua, is_mobile): 
+    from user_agents import parse
     user_agent = parse(ua)
     model = user_agent.device.model
     platform = user_agent.os.family
@@ -122,77 +127,67 @@ def read_agents():
 def make_driver():
     try:
         pc_device = ["1920,1440","1920,1200","1920,1080","1600,1200","1600,900",
-                        "1536,864", "1440,1080","1440,900","1360,768"
-                ]
-
-        mo_device = [
-                    "360,640", "360,740", "375,667", "375,812", "412,732", "412,846",
-                    "412,869", "412,892", "412,915"
-                ]
-
-        width,height = random.choice(mo_device).split(",")
+                     "1536,864", "1440,1080","1440,900","1360,768"]
+        mo_device = ["360,640", "360,740", "375,667", "375,812", "412,732", "412,846",
+                     "412,869", "412,892", "412,915"]
+        width, height = random.choice(mo_device).split(",")
         UA_list = read_agents()
         UA = random.choice(UA_list)
-        print("여기까지완료1")
         options = uc.ChromeOptions()
         options.add_argument(f'--user-agent={UA}')
         options.add_argument(f"--window-size={width},{height}")
         options.add_argument("--no-first-run --no-service-autorun --password-store=basic")
         options.add_argument('--disable-logging')
         options.add_argument('--disable-popup-blocking')
-        # linux 환경에서 필요한 option + 속도 개선
-        options.add_argument("--headless") # GUI 없이 실행
-        options.add_argument('--no-sandbox') # Browser가 보안 취약점에 노출될 가능성을 최소화하기 위해 사용되는 Sandbox 보호 기능을 해제(대신 속도 상승)
-        options.add_argument('--disable-dev-shm-usage') # 공유 메모리 파일 시스템 크기를 제한하지 않게 설정하는 것
-        #options.add_argument('--disable-gpu')  # GPU 가속 비활성화 -> 메모리 문제 해결을 위해 임시사용
+        options.add_argument("--headless")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
 
-        print("여기까지완료2")
-        driver = uc.Chrome(executable_path=config['chromedriver_path'],options=options)
-        print("여기까지완료3")
+        driver = uc.Chrome(executable_path=config['chromedriver_path'], options=options)
         UA_Data = make_user_agent(UA, True)
-        print("여기까지완료4")
         driver.execute_cdp_cmd("Network.setUserAgentOverride", UA_Data)
-        print("여기까지완료5")
-
         Mobile = {"enabled":True, "maxTouchPoints" : random.choice([1,5])}
         driver.execute_cdp_cmd("Emulation.setTouchEmulationEnabled", Mobile)
-        driver.execute_cdp_cmd("Emulation.setNavigatorOverrides",{"platform":"Linux armv8l"})
-
+        driver.execute_cdp_cmd("Emulation.setNavigatorOverrides", {"platform":"Linux armv8l"})
         driver.execute_cdp_cmd("Emulation.setDeviceMetricsOverride", {
             "width":int(width),
             "height":int(height),
             "deviceScaleFactor":1,
             "mobile":True,
         })
-
-        def generate_random_geolcation():
-            ltop_lat = 37.61725745260699
-            ltop_long = 126.92437164480178
-            rbottom_lat = 37.56825361755575
-            rbottom_long = 127.05771697149082
-
-            targetLat = random.uniform(rbottom_lat, ltop_lat)
-            targetLong = random.uniform(ltop_long, rbottom_long)
-            return {"latitude" : targetLat, "longitude" : targetLong, "accuracy":100}
-        
-        GEO_DATA = generate_random_geolcation()
+        GEO_DATA = {"latitude": random.uniform(37.56825361755575, 37.61725745260699),
+                    "longitude": random.uniform(126.92437164480178, 127.05771697149082),
+                    "accuracy":100}
         driver.execute_cdp_cmd("Emulation.setGeolocationOverride", GEO_DATA)
-        print("여기까지완료6")
-        driver.execute_cdp_cmd("Emulation.setUserAgentOverride",UA_Data)
-        driver.set_window_size(int(width),int(height))
-        print("여기까지완료7")
+        driver.execute_cdp_cmd("Emulation.setUserAgentOverride", UA_Data)
+        driver.set_window_size(int(width), int(height))
         return driver
-        
     except Exception as e:
-        print(e)
-        driver = None
-        return driver
+        print(f"Driver 생성 중 오류 발생: {e}")
+        return None
 
-driver = make_driver()
+async def fetch_page(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
-print("==================================================")
-print(driver)
-print("==================================================")
+async def parse_page(html, base_url):
+    soup = BeautifulSoup(html, 'html.parser')
+    posts = []
+    for post in soup.select('.td-subject > a'):
+        url = post.get('href')
+        subject = post.select_one('strong').text
+        
+        if url:
+            # URL이 상대 경로일 경우 base_url과 결합
+            if not url.startswith('http'):
+                # base_url의 끝에 있는 슬래시를 제거하고, 상대 URL의 시작에 있는 슬래시를 제거합니다.
+                # ex)  https://www.sungkyul.ac.kr/skukr/354/
+                # ->   https://www.sungkyul.ac.kr/skukr/354로 변환
+                url = base_url+url
+        
+        posts.append((subject, url))
+    return posts
+
 # ========================================== 봇 이벤트 및 명령어 ====================================
 
 @bot.event
@@ -214,64 +209,38 @@ async def check_notices():
     print(f"{attempt}번째 시도 중입니다.")
     # await channel.send(f"{attempt}번째 시도 중입니다.") # 임시
 
-    for sku_site_link in sku_site_links:
-        try:
-            driver.get(sku_site_link)
-        except Exception as e:
-            print(f"[ERROR 001] 성결대학교 홈페이지 오류입니다. {sku_site_link} 페이지를 띄우지 못했습니다.")
-            print(f"에러명은 아래와 같음")
-            print(f"{e}")
-            # await channel.send(f'채팅 서버 개발자님 확인해 주세요! 봇이 너무 아파요 ㅜ_ㅜ \n 에러 코드 : [ERROR 001] 성결대학교 홈페이지 오류입니다. {sku_site_link} 페이지를 확인하지 못했습니다. \n 곧 채팅 서버 관리자가 나타나서 밤샘 작업을 하여 정상화할 예정이에요. 이용에 불편을 드려서 죄송합니다.')
+    async with aiohttp.ClientSession() as session:
+        for sku_site_link in sku_site_links:
+            try:
+                html = await fetch_page(session, sku_site_link)
+                base_url = "https://www.sungkyul.ac.kr" # 기본 URL을 생성
+                posts = await parse_page(html, base_url)
+                cur_subjects[homepage_num] = posts
+            except Exception as e:
+                print(f"[ERROR] 페이지 처리 중 오류 발생: {e}")
 
-        driver.implicitly_wait(200)
+            different_subject_cnt = 0
+            if attempt != 1 and cur_subjects[homepage_num] != before_subjects[homepage_num]:
+                for subject, url in cur_subjects[homepage_num]:
+                    if (subject, url) not in before_subjects[homepage_num]:
+                        different_subject_cnt += 1
 
-        try:
-            post_elements = driver.find_elements(By.CSS_SELECTOR, '.td-subject > a')
-        except:
-            print("[ERROR 002] 성결대학교 상위 10개 공지 전체 element CSS_SELECTOR 오류입니다.")
-            # await channel.send(f'채팅 서버 개발자님 확인해 주세요! 봇이 너무 아파요 ㅜ_ㅜ \n [ERROR 002] 성결대학교 상위 10개 공지 전체 element CSS_SELECTOR 오류입니다. \n 곧 채팅 서버 관리자가 나타나서 밤샘 작업을 하여 정상화할 예정이에요. 이용에 불편을 드려서 죄송합니다.')
+            if attempt != 1 and cur_subjects[homepage_num] != before_subjects[homepage_num] and different_subject_cnt < 7:
+                for subject, url in cur_subjects[homepage_num]:
+                    if (subject, url) not in before_subjects[homepage_num]:
+                        print(f"새로운 공지 제목 : {subject}")
+                        print(f"새로운 공지 URL : {url.strip()}")
+                        await channel.send(f'새로운 공지 제목 : {subject} \n {url.strip()}')
 
-        try:
-            for post_element in post_elements:
-                url = post_element.get_attribute('href')
-                subject_element = post_element.find_element(By.CSS_SELECTOR, 'strong')
-                subject = subject_element.text
-                cur_subjects[homepage_num].append((subject, url))
-        except:
-            print("[ERROR 003] 성결대학교 게시물의 제목이나 URL을 받아오지 못했습니다.")
-            # await channel.send(f'채팅 서버 개발자님 확인해 주세요! 봇이 너무 아파요 ㅜ_ㅜ \n [ERROR 003] 성결대학교 게시물의 제목이나 URL을 받아오지 못했습니다. \n 곧 채팅 서버 관리자가 나타나서 밤샘 작업을 하여 정상화할 예정이에요. 이용에 불편을 드려서 죄송합니다.')
+            before_subjects[homepage_num] = copy.deepcopy(cur_subjects[homepage_num])
+            cur_subjects[homepage_num].clear()
 
-        # 2024-07-14 추가 : 바뀐 공지사항 갯수 확인
-        different_subject_cnt = 0 
-        if attempt != 1 and cur_subjects[homepage_num] != before_subjects[homepage_num]:
-            for subject,url in cur_subjects[homepage_num]:
-                if (subject, url) not in before_subjects[homepage_num]:
-                    different_subject_cnt += 1
+            homepage_num += 1
+            if homepage_num == 12:
+                homepage_num -= 12
 
-        # 2024-07-14 추가 : 한 번에 바뀐 공지사항이 7개 이상인 경우는 적으므로 이런 경우 오류로 보고 디스코드에 알림을 보내지 않는다.
-        # 2024-07-14 추가 : 한 번에 바뀐 공지사항이 7개 미만인 경우에만 디스코드에 알림을 보내준다.
-        if attempt != 1 and cur_subjects[homepage_num] != before_subjects[homepage_num] and different_subject_cnt < 7:
-            for subject,url in cur_subjects[homepage_num]:
-                if (subject, url) not in before_subjects[homepage_num]:
-                    print(f"새로운 공지 제목 : {subject}")
-                    print(f"새로운 공지 URL : {url.strip()}")
-                    await channel.send(f'새로운 공지 제목 : {subject} \n {url.strip()}')
-
-        # await channel.send(f'{cur_subjects[homepage_num]}')#임시
-        # await channel.send(f'바뀐 공지 개수 : {different_subject_cnt}') # 임시 
-
-        before_subjects[homepage_num] = copy.deepcopy(cur_subjects[homepage_num])
-        cur_subjects[homepage_num].clear()
-        
-        homepage_num += 1
-        if(homepage_num == 12):
-            homepage_num -= 12
-
-
-        await asyncio.sleep(random.uniform(5.0, 10.0)) # 여기 조정
+            await asyncio.sleep(random.uniform(5.0, 10.0))
 
     attempt += 1
-
-    
 
 bot.run(config['discord_token'])
